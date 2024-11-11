bb4c5d3cbb114f4573b2a3be96bc90952bdad4f1
chikamura <mchikamura01@gmail.com>
2024-11-11 16:36:45 +0900

    add test

--- src/test/java/org/embulk/output/sf_bulk_api/TestSForceColumnVisitor.java
+++ src/test/java/org/embulk/output/sf_bulk_api/TestSForceColumnVisitor.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import com.sforce.soap.partner.sobject.SObject;
+import java.util.Calendar;
 import org.embulk.spi.Column;
 import org.embulk.spi.PageReader;
 import org.embulk.spi.type.Types;
@@ -36,5 +37,19 @@ public class TestSForceColumnVisitor {
 
     visitor.doubleColumn(column);
     assertEquals(1.1, record.getField("test"));
+  }
+
+  @SuppressWarnings("deprecation") // For the use of org.embulk.spi.time.Timestamp.
+  @Test
+  public void testTimestampColumnNotNull() {
+    final SObject record = new SObject();
+    final Column column = new Column(0, "test", Types.TIMESTAMP);
+    final SForceColumnVisitor visitor = new SForceColumnVisitor(record, pageReader, false);
+
+    doReturn(false).when(pageReader).isNull(column);
+    doReturn(org.embulk.spi.time.Timestamp.ofEpochMilli(100)).when(pageReader).getTimestamp(column);
+
+    visitor.timestampColumn(column);
+    assertEquals(100, ((Calendar) record.getField("test")).getTimeInMillis());
   }
 }
